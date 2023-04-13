4fa9e8feb8c0a47a35bb1b7ab11e9cfc5d214273
emilymerrow <emilyanddavidmerrow@gmail.com>
2023-04-13 17:14:26 -0400

    lab done

--- main_app/models.py
+++ main_app/models.py
@@ -5,20 +5,60 @@ from django.urls import reverse
 from django.urls import reverse
 
 # Create your models here.
+
+
+class Flower(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    blooming_season = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('flowers_detail', kwargs={'pk': self.id})
+    
+
 class Butterfly(models.Model):
 
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     wingspan = models.IntegerField(blank=True, null=True)
+    flowers = models.ManyToManyField(Flower)
 
 
-    def __str__(self):
-        return self.name
-    
     def get_absolute_url(self):
         # path('cats/<int:cat_id>/', views.cats_detail, name='detail'),
         # self.id is referring to the cat that was just created
         # when the submit the form
         return reverse('detail', kwargs={'butterfly_id': self.id})
 
+    def __str__(self):
+        return f"{self.name} has {self.wingspan} mm long"
+    
+  
+    
+
+NECTAR_SOURCES = (
+    ('B', 'Blossom'),
+    ('T', 'Tree Sap'),
+    ('Ft', 'Fruit'),
+    ('Ot', 'Others'),
+)
+
+class NectarSource(models.Model):
+    date = models.DateField('nectar source date')
+    # source will be represented by a single letter (F)lowers, (T)ree Sap, (Ft)ruit, (Ot)hers
+    source = models.CharField(max_length=2, choices=NECTAR_SOURCES, default=NECTAR_SOURCES[0][0])
+    # Create a butterfly_id FK
+    # models.CASCADE, if we delete a butterfly, delete its nectar sources as well
+    butterfly = models.ForeignKey(Butterfly, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        # get_source_display is automatically generated,
+        # on inputs that have choices parameter, see source
+        return f"{self.get_source_display()} on {self.date}"
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,27 @@
 from django.urls import path
-from main_app import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('butterfly/', views.butterfly_index, name='index'),
-    path('butterfly/<int:butterfly_id>/', views.butterfly_detail, name='detail'),
-    path('butterfly/create', views.ButterflyCreate.as_view(), name='butterflies_create'),
 
+    path('butterflies/', views.butterfly_index, name='index'),
+
+    path('butterflies/<int:butterfly_id>/', views.butterfly_detail, name='detail'),
+
+    path('butterflies/create', views.ButterflyCreate.as_view(), name='butterflies_create'),
+
+    path('butterflies/<int:pk>/update/', views.ButterflyUpdate.as_view(), name='butterflies_update'),
+    
+    path('butterflies/<int:pk>/delete/', views.ButterflyDelete.as_view(), name='butterflies_delete'),
+    
+    path('butterflies/<int:butterfly_id>/add_nectarSource/', views.add_nectar_source, name='add_nectarSource'),
+    
+    path('butterflies/<int:butterfly_id>/assoc_flower/<int:flower_id>/', views.assoc_flower, name='assoc_flower'),
+
+    path('flowers/', views.FlowerList.as_view(), name='flowers_index'),
+    path('flowers/<int:pk>/', views.FlowerDetail.as_view(), name='flowers_detail'),
+    path('flowers/create/', views.FlowerCreate.as_view(), name='flowers_create'),
+    path('flowers/<int:pk>/update/', views.FlowerUpdate.as_view(), name='flowers_update'),
+    path('flowers/<int:pk>/delete/', views.FlowerDelete.as_view(), name='flowers_delete'),
 ]
